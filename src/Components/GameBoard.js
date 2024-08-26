97831c5cb8ee97945f3237d1d9448df65ec5f44f
Amd-Muje <ahmadmujahid343@gmail.com>
2024-08-26 19:38:51 +0800

    starting make React Game

---
+++ src/Components/GameBoard.js
@@ -0,0 +1,19 @@
+import React from "react";
+import GameCircle from "./GameCircle";
+
+const GameBoard = () => {
+    return (
+    <div>
+        <GameCircle/>
+        <GameCircle/>
+        <GameCircle/>
+        <GameCircle/>
+        <GameCircle/>
+        <GameCircle/>
+        <GameCircle/>
+        <GameCircle/>
+    </div>
+    )
+}
+
+export default GameBoard;
